c31676dc1a59de8d978488bd56d07367fee249f1
GourabNexus <gourabmaity2004@gmail.com>
2026-04-04 01:59:39 +0530

    fix socket connection

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://whiteboard-app-sikc.onrender.com");
+export const socket = io("https://whiteboard-app-sikc.onrender.com", {
+  transports: ["websocket"], // ✅ force websocket
+  withCredentials: true,
+});
