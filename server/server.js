13134d5a06583fb3139b6de9e9ad81659d4b3d38
GourabNexus <gourabmaity2004@gmail.com>
2026-04-04 01:41:47 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: [
+  "http://localhost:3000",
+  "https://whiteboard-app-psi-ten.vercel.app/"
+],
   },
 });
 
